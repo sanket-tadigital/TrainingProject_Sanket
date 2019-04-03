ad0837d892fd9da050043b152e461bbb61890af1
Sanket Sinha <training.project.sanket@gmail.com>
2019-04-03 19:37:13 +0530

    updated entity module with new setter and getter functions

--- src/com/tadigital/ecommerce/customer/entity/Customer.java
+++ src/com/tadigital/ecommerce/customer/entity/Customer.java
@@ -6,13 +6,13 @@ public class Customer {
 	private String lname;
 	private String address;
 	private String gender;
-	private long zip;
+	private String zip;
 	private String city;
 	private String country;
 	private String state;
 	private String email;
 	private String password;
-	private long contact;
+	private String contact;
 	
 	/*public Customer(int id, String fname, String lname, String address, String city, String gender, long zip, String country, String state, String email, String password, long contact)
 	{
@@ -49,6 +49,28 @@ public class Customer {
 	public String getpassword() {
 		return password;
 	}
+	public String getgender() {
+		return gender;
+	}
+	public String getaddress() {
+		return address;
+	}
+	public String getzip() {
+		return zip;
+	}
+	public String getcontact() {
+		return contact;
+	}
+	public String getcity() {
+		return city;
+	}
+	public String getcountry() {
+		return country;
+	}
+	public String getstate() {
+		return state;
+	}
+
 	
 	public void setfname(String fname)
 	{
@@ -71,5 +93,26 @@ public class Customer {
 	public void setpassword(String password)
 	{
 		this.password=password;
+	}
+	public void setgender(String gender) {
+		this.gender=gender;
+	}
+	public void setaddress(String address) {
+		this.address=address;
+	}
+	public void setzip(String zip) {
+		this.zip=zip;
+	}
+	public void setcontact(String contact) {
+		this.contact=contact;
+	}
+	public void setcity(String city) {
+		this.city=city;
+	}
+	public void setcountry(String country) {
+		this.country=country;
+	}
+	public void setstate(String state) {
+		this.state=state;
 	}
 }
