1222897d0177c4462774e148f1eda62cdfa5e572
Sanket Sinha <training.project.sanket@gmail.com>
2019-04-05 22:53:49 +0530

    updated with proper comments and indentation

--- src/com/tadigital/ecommerce/customer/controller/PasswordChangeControllerServlet.java
+++ src/com/tadigital/ecommerce/customer/controller/PasswordChangeControllerServlet.java
@@ -18,45 +18,42 @@ public class PasswordChangeControllerServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		/*TAKING DATA FROM FORM*/
+		String oldpass = (String) req.getParameter("t1");
+		String newpass = (String) req.getParameter("t2");
+		String passretype = (String) req.getParameter("t3");
 		
-		String oldpass=(String)req.getParameter("t1");
-		String newpass=(String)req.getParameter("t2");
-		String passretype=(String)req.getParameter("t3");
-		HttpSession ses=req.getSession();
-		Customer c1=(Customer)ses.getAttribute("CUSTOMERDATA");
-		String email=c1.getemail();
-		Customer customer=new Customer();
-		System.out.println(oldpass+" "+newpass+" "+passretype);
-		if(newpass.equals(passretype))
-		{
-			customer.setpassword(newpass);
+		
+		HttpSession session = req.getSession();
+		Customer c1 = (Customer) session.getAttribute("CUSTOMERDATA");	
+		//CHECKING SESSION VARIABLE TO GET EMAIL OF USER
+		String email = c1.getemail();														
+		Customer customer = new Customer();
+	
+		//CHECKING ENTRIES AT RETYPE AND NEW PASSWORD FORM FIELDS
+		if (newpass.equals(passretype)) {													
+			customer.setpassword(newpass);								
 			customer.setemail(email);
 			CustomerService customerService = new CustomerService();
-			boolean status = customerService.ChangePassword(customer,oldpass);	
-			String status2=customerService.sendSecurityMail(c1.getfname(),c1.getemail());
-			if(status && status2=="SENT")
-			{
-				
-				ses.setAttribute("check","changesuccess");
+			boolean status = customerService.ChangePassword(customer, oldpass);	
+			//CALLING SERVICE TO CHANCE PASSWORD
+			String status2 = customerService.sendSecurityMail(c1.getfname(), c1.getemail());
+			if (status && status2 == "SENT") {
+				session.setAttribute("check", "changesuccess");								
+				//SETTING SESSION VARIABLE TO CHECK SUCCESSFUL PASSWORD CHANGE
+				RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
+				rd.forward(req, resp);
+			} else {
+				session.setAttribute("check", "changefail");
 				RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
 				rd.forward(req, resp);
 			}
-			else
-			{
-				
-				ses.setAttribute("check","changefail");
-				RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
-				rd.forward(req, resp);
-			}
-		}
-		else
-		{
-			
-			ses.setAttribute("check","changefail");
+		} else {
+			session.setAttribute("check", "changefail");
 			RequestDispatcher rd = req.getRequestDispatcher("CustomerAccount.jsp");
 			rd.forward(req, resp);
 		}
-		
+
 	}
 }
-
