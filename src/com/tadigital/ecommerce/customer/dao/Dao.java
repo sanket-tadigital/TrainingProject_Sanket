7e2e9647fe684b53dd60e7862fe7e29170a40828
Sanket Sinha <training.project.sanket@gmail.com>
2019-04-05 22:56:25 +0530

    updated with proper comments and indentation

--- src/com/tadigital/ecommerce/customer/dao/Dao.java
+++ src/com/tadigital/ecommerce/customer/dao/Dao.java
@@ -1,4 +1,5 @@
 package com.tadigital.ecommerce.customer.dao;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -8,7 +9,7 @@ import java.sql.Statement;
 public class Dao {
 	protected Connection openConnection() {
 		Connection con = null;
-		
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/tb_db", "root", "");
@@ -17,45 +18,45 @@ public class Dao {
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
-		
+
 		return con;
 	}
-	
+
 	protected Statement openStatement(Connection con) {
 		Statement stmt = null;
-		
+
 		try {
 			stmt = con.createStatement();
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
-		
+
 		return stmt;
 	}
-	
+
 	protected void closeResultSet(ResultSet rs) {
 		try {
-			if(rs != null) {
+			if (rs != null) {
 				rs.close();
 			}
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 	}
-	
+
 	protected void closeStatement(Statement stmt) {
 		try {
-			if(stmt != null) {
+			if (stmt != null) {
 				stmt.close();
 			}
 		} catch (SQLException sqle) {
 			sqle.printStackTrace();
 		}
 	}
-	
+
 	protected void closeConnection(Connection con) {
 		try {
-			if(con != null) {
+			if (con != null) {
 				con.close();
 			}
 		} catch (SQLException sqle) {
