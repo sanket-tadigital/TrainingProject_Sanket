b617aa5c60c62555d8ebe6e35f7e04d2b556c914
Sanket Sinha <training.project.sanket@gmail.com>
2019-04-05 22:56:51 +0530

    updated with proper comments and indentation

--- src/com/tadigital/ecommerce/customer/entity/Customer.java
+++ src/com/tadigital/ecommerce/customer/entity/Customer.java
@@ -13,106 +13,110 @@ public class Customer {
 	private String email;
 	private String password;
 	private String contact;
-	
-	/*public Customer(int id, String fname, String lname, String address, String city, String gender, long zip, String country, String state, String email, String password, long contact)
-	{
-		this.id=id;
-		this.fname=fname;
-		this.lname=lname;
-		this.address=address;
-		this.gender=gender;
-		this.zip=zip;
-		this.country=country;
-		this.state=state;
-		this.city=city;
-		this.email=email;
-		this.password=password;
-		this.contact=contact;
-	}*/
-	
+	private long lastlogin;
+
 	public int getid() {
 		return id;
 	}
-	
+
 	public String getfname() {
 		return fname;
 	}
-	
+
 	public String getlname() {
 		return lname;
 	}
-	
+
 	public String getemail() {
 		return email;
 	}
-	
+
 	public String getpassword() {
 		return password;
 	}
+
 	public String getgender() {
 		return gender;
 	}
+
 	public String getaddress() {
 		return address;
 	}
+
 	public String getzip() {
 		return zip;
 	}
+
 	public String getcontact() {
 		return contact;
 	}
+
 	public String getcity() {
 		return city;
 	}
+
 	public String getcountry() {
 		return country;
 	}
+
 	public String getstate() {
 		return state;
 	}
 
-	
-	public void setfname(String fname)
-	{
-		this.fname=fname;
+	public long getlastlogin() {
+		return lastlogin;
 	}
-	public void setid(int id)
-	{
-		this.id=id;
+
+	public void setfname(String fname) {
+		this.fname = fname;
 	}
-	public void setlname(String lname)
-	{
-		this.lname=lname;
+
+	public void setid(int id) {
+		this.id = id;
 	}
-	
-	public void setemail(String email)
-	{
-		this.email=email;
+
+	public void setlname(String lname) {
+		this.lname = lname;
 	}
-	
-	public void setpassword(String password)
-	{
-		this.password=password;
+
+	public void setemail(String email) {
+		this.email = email;
 	}
+
+	public void setpassword(String password) {
+		this.password = password;
+	}
+
 	public void setgender(String gender) {
-		this.gender=gender;
+		this.gender = gender;
 	}
+
 	public void setaddress(String address) {
-		this.address=address;
+		this.address = address;
 	}
+
 	public void setzip(String zip) {
-		this.zip=zip;
+		this.zip = zip;
 	}
+
 	public void setcontact(String contact) {
-		this.contact=contact;
+		this.contact = contact;
 	}
+
 	public void setcity(String city) {
-		this.city=city;
+		this.city = city;
 	}
+
 	public void setcountry(String country) {
-		this.country=country;
+		this.country = country;
 	}
+
 	public void setstate(String state) {
-		this.state=state;
+		this.state = state;
 	}
+
+	public void setlastlogin(long lastlogin) {
+		this.lastlogin = lastlogin;
+	}
+
 }
